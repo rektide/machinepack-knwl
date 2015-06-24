1cb260c4fd45995d5c2803752b239b56ac60a74d
rektide <rektide@voodoowarez.com>
2015-06-23 22:19:41 -0400

    Make-knwl-machine pattern to turn knwl plugins into machines.

---
+++ machines/get-dates.js
@@ -0,0 +1,5 @@
+module.exports= require("../make-knwl-machine")(
+	"dates", 
+	"One of the best discourses happened on November 19th, 1863, on bringing forth a nation of liberty, dedicated to equality",
+	"A string which will be parsed for dates"
+)
---
+++ make-knwl-machine.js
@@ -0,0 +1,45 @@
+var knwl= require("knwl")
+
+var registered= {}
+
+module.exports = function(name, example, description, plugin){
+	return {
+		friendlyName: "Get " + name,
+		description: "Get " + name+ " from strings",
+		inputs: {
+			string: {
+				example: example,
+				description: description,
+				required: true
+			
+			},
+			language: {
+				example: "english",
+				description: "Language parameter to pass to knwl",
+				required: false
+			}
+		},
+		defaultExit: "success",
+		exits: {
+			error: {
+				description: "No + " name + " found."
+			},
+			success: {
+				description: "All found " + name + "."
+			}
+		},
+		fn: function(inputs, exits){
+			var parser= knwl(inputs.language)
+			if(plugin){
+				parser.register(name, plugin)
+			}
+			parser.init(inputs.string)
+			var results = parser.get(name)
+			if(results && results.length){
+				exits.success(results)
+			}else{
+				exits.error()
+			}
+		}
+	}
+}
